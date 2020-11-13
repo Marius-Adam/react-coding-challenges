1641b5604443452e6eefe17530e54e44664318a0
Marius-Adam <marius.c.adam@gmail.com>
2020-11-13 17:36:24 +0000

    api call and populated playlists

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: 'd05b36e30abc4d5b9438ccebebf0d74b',
+    clientSecret: '1ec6a889e35142a7aa130638c0e803d6'
   }
 }
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import makeRequest from "../api/makeRequest";
+
+/*
+  Solution: called the makeRequest() helper method with the provided paths on componentDidMount and set the state to the response 
+*/
 
 export default class Discover extends Component {
   constructor() {
@@ -9,18 +14,53 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
     };
   }
+
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    try {
+      const releases = await makeRequest('new-releases');
+      this.setState({newReleases: releases.data.albums.items})
+
+      const featured = await makeRequest('featured-playlists');
+      this.setState({playlists: featured.data.playlists.items})
+
+      const category = await makeRequest('categories');
+      this.setState({categories: category.data.categories.items})
+    }
+    catch (error) {
+      console.log(error);
+
+  }
+}
 
   render() {
     const { newReleases, playlists, categories } = this.state;
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }
